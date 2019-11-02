91c56aaf684a0fb03fc4b0086c88c30bec38e0c7
Jatin Partap Rana <>
2019-11-02 00:04:23 -0300

    no message

---
+++ build/precache-manifest.a62f1e253fb43a7f243ee2104bafcc5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0793c73e00fa2b2ef54999b17a2c6967",
+    "url": "/monosters-rolodex/index.html"
+  },
+  {
+    "revision": "d9b5552208773aae6b08",
+    "url": "/monosters-rolodex/static/css/main.3b738214.chunk.css"
+  },
+  {
+    "revision": "b9923b6180899e717bdf",
+    "url": "/monosters-rolodex/static/js/2.bd2f63ac.chunk.js"
+  },
+  {
+    "revision": "d9b5552208773aae6b08",
+    "url": "/monosters-rolodex/static/js/main.bfac82c9.chunk.js"
+  },
+  {
+    "revision": "17d548c7013a4817c040",
+    "url": "/monosters-rolodex/static/js/runtime-main.c297a9e5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monosters-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
